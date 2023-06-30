20d8e853ae4e371e6cf31d29e419b0afb62c60b3
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-30 20:37:38 +0300

    feat: add hint for allowed symbols

---
+++ src/app/password-field/constants/constants.ts
@@ -0,0 +1,3 @@
+export const allowedNumbers = '0123456789';
+export const allowedSymbols = '@#/!%&';
+export const allowedLetters = 'abcdefghijklmnopqrstuvwxyz';
--- src/app/password-field/password-field.component.ts
+++ src/app/password-field/password-field.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { allowedSymbols } from "./constants/constants";
 @Component({
   selector: 'app-password-field',
   standalone: true,
@@ -16,10 +17,21 @@ import { FormControl, ReactiveFormsModule } from "@angular/forms";
       <div class="easy">The password is easy.</div>
       <div class="medium">The password is medium.</div>
       <div class="strong">The password is strong.</div>
+      <div class="hint">
+        Allowed english characters, numbers and symbols:
+        <span>{{ symbols }}</span
+        >.
+      </div>
     </form>
   `,
   styleUrls: ['./password-field.component.scss'],
 })
 export class PasswordFieldComponent {
   passwordControl = new FormControl('');
+  symbols = allowedSymbols.split('').join(', ');
+
+  minLengthValidator() {}
+  easyPasswordValidator() {}
+  mediumPasswordValidator() {}
+  strongPasswordValidator() {}
 }
