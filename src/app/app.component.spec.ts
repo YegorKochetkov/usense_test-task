b859d662e572296c4b754af7f6efec94fcf05145
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-30 18:54:27 +0300

    feat: update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { PasswordFieldComponent } from "./password-field/password-field.component";
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [PasswordFieldComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -22,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('usense_test-task app is running!');
+    expect(compiled.querySelector('.content h1')?.textContent).toContain(
+      'USENSE_TEST-TASK'
+    );
   });
 });
---
+++ src/app/password-field/password-field.component.spec.ts
@@ -0,0 +1,34 @@
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { PasswordFieldComponent } from "./password-field.component";
+
+describe('PasswordFieldTempComponent', () => {
+  let component: PasswordFieldComponent;
+  let fixture: ComponentFixture<PasswordFieldComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PasswordFieldComponent],
+    });
+    fixture = TestBed.createComponent(PasswordFieldComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should update the value of the input field', () => {
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('.password');
+    const event = new Event('input');
+
+    input.value = 'Red';
+    input.dispatchEvent(event);
+
+    expect(component.passwordControl.value).toEqual('Red');
+  });
+});
