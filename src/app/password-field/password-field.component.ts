ab95e99a4f50727f8687b618d783ff54d40c63de
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-30 18:54:14 +0300

    feat: add password strength info fields set formControl for password input

--- src/app/password-field/password-field.component.ts
+++ src/app/password-field/password-field.component.ts
@@ -1,15 +1,25 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 @Component({
   selector: 'app-password-field',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <form class="form">
-      <input type="password" placeholder="password" class="password" />
+      <input
+        type="password"
+        placeholder="password"
+        class="password"
+        [formControl]="passwordControl"
+      />
+      <div class="easy">The password is easy.</div>
+      <div class="medium">The password is medium.</div>
+      <div class="strong">The password is strong.</div>
     </form>
   `,
   styleUrls: ['./password-field.component.scss'],
 })
-export class PasswordFieldComponent {}
+export class PasswordFieldComponent {
+  passwordControl = new FormControl('');
+}
