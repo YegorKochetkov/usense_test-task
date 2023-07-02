2d299bc1e233ec84fb50866188bdcf8056d247fd
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-02 11:40:35 +0300

    refactor: move & update constants

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,8 @@
+export const ALLOWED_NUMBERS = '0123456789';
+export const ALLOWED_SPEC_SYMBOLS = '!@#$%&?';
+export const ALLOWED_LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+export const ALLOWED_NUMBERS_REGEXP = /([0-9])+/i;
+export const ALLOWED_LETTERS_REGEXP = /([a-z])+/i;
+export const ALLOWED_SPEC_SYMBOLS_REGEXP = /([!@#$%&?])+/i;
+export const MIN_LENGTH = 8;
+export const MAX_LENGTH = 64;
--- src/app/password-field/constants/constants.ts
+++
@@ -1,3 +0,0 @@
-export const allowedNumbers = '0123456789';
-export const allowedSymbols = '@#/!%&';
-export const allowedLetters = 'abcdefghijklmnopqrstuvwxyz';
