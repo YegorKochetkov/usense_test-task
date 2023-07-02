692e4c9eb63505d2d8b0209feb3398c5117fe201
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-02 11:51:09 +0300

    feat: update form add formGroup, validators, submit button

--- src/app/password-field/password-field.component.ts
+++ src/app/password-field/password-field.component.ts
@@ -1,8 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { allowedSymbolsRegexpValidator } from '../shared/allowed-symbols-regexp.directive';
+import { minLengthValidator } from '../shared/min-length.directive';
 import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  ALLOWED_LETTERS_REGEXP,
+  ALLOWED_NUMBERS_REGEXP,
   ALLOWED_SPEC_SYMBOLS,
+  ALLOWED_SPEC_SYMBOLS_REGEXP,
   MAX_LENGTH,
   MIN_LENGTH,
 } from '../constants/constants';
@@ -12,16 +22,22 @@ import {
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <form class="form">
-      <input
-        type="password"
-        placeholder="password"
-        class="password"
-        [formControl]="passwordControl"
-      />
+    <form [formGroup]="passwordForm" class="form">
+      <div class="inputField">
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          class="password"
+          formControlName="passwordControl"
+        />
+        <button type="submit" class="button">Submit</button>
+      </div>
+
       <div class="easy">The password is easy.</div>
       <div class="medium">The password is medium.</div>
       <div class="strong">The password is strong.</div>
+
       <div class="hint">
         Allowed english characters, numbers and symbols:
         <span> {{ symbols }} </span>
@@ -34,7 +50,17 @@ import {
   styleUrls: ['./password-field.component.scss'],
 })
 export class PasswordFieldComponent {
-  passwordControl = new FormControl('');
+  passwordForm = new FormGroup({
+    passwordControl: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(MAX_LENGTH),
+      minLengthValidator(MIN_LENGTH),
+      allowedSymbolsRegexpValidator('allowedLetters', ALLOWED_LETTERS_REGEXP),
+      allowedSymbolsRegexpValidator('numbers', ALLOWED_NUMBERS_REGEXP),
+      allowedSymbolsRegexpValidator('specSymbols', ALLOWED_SPEC_SYMBOLS_REGEXP),
+    ]),
+  });
+
   symbols = ALLOWED_SPEC_SYMBOLS.split('').join(', ');
   minLength = MIN_LENGTH;
   maxLength = MAX_LENGTH;
